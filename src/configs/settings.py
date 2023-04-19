14f74927f14be494cd91698421478f5fbbec525c
Merk Vladimir <merkvladimir@gmail.com>
2023-04-19 14:05:31 +0300

    установлен Debug Toolbar

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "menu.apps.MenuConfig",
     "blog.apps.BlogConfig",
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "configs.urls"
@@ -126,3 +128,7 @@ STATICFILES_DIRS = ['static/']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
